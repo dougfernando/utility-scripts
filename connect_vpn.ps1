455cfc06d26500c9e844aabbff69a8389ef3b295
Douglas Fernando da Silva <doug.fernando@gmail.com>
2009-06-22 23:47:19 -0300

    attempt-try code added

--- connect_vpn.ps1
+++ connect_vpn.ps1
@@ -1,3 +1,31 @@
+echo "CONNECTING TO CHEMTECH VPN..."
+echo "---------------------------------------------"
+echo ""
+echo "Going to command line mode..."
 & "C:\Program Files\CheckPoint\SecuRemote\bin\scc.exe" setmode cli
-& "C:\Program Files\CheckPoint\SecuRemote\bin\scc.exe" connect -p "200.173.214.10"
-& "C:\users\douglas\scripts\use_hosts_chemtech.ps1"
+echo ""
+
+$attempt = 0
+$maxNumberOfAttempts = 5
+
+$success = $false
+do {
+  echo "Connecting to server: 200.173.214.10..."
+  $outp = & "C:\Applications\CheckPoint\SecuRemote\bin\scc.exe" connect -p "200.173.214.10";
+  echo $outp
+  foreach ($item in $outp) { 
+    if ($item.ToString().ToUpper().IndexOf("SUCCEEDED") -ne -1) {
+      $success = $true;
+      break
+    }
+  }
+  if ($success) {
+    echo "Connection was OK!";
+    echo "Changing the host file to chemtech mode...";
+    & "C:\users\douglas\scripts\use_hosts_chemtech.ps1"
+  } else {
+    echo "Connection was NOK, trying again.";
+    $attempt = $attempt + 1
+  }
+} while(-not $success -and $attempt -le $maxNumberOfAttempts);
+
